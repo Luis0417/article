df36bb89ac22091011be2bc924db56e862d4d448
Luis0417 <62229028+Luis0417@users.noreply.github.com>
2021-01-11 04:04:00 +0800

    Update route structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,15 +23,16 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    @if (auth()->id())
+                        <a class="navbar" href="{{ url('/home') }}">
+                            {{ __('Home') }}
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                            <span class="navbar-toggler-icon"></span>
+                    </button>
+                    @endif
                     <ul class="navbar-nav mr-auto">
                         @hasrole('admin')
                         <li class="nav-item">
