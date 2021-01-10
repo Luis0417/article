7fe7826c9309a3cbee819b0785bc6cfbcb9eb15e
Luis0417 <62229028+Luis0417@users.noreply.github.com>
2021-01-11 03:28:52 +0800

    Update UsersTableSeeder.php
    Remove seeder for contributor user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,13 +24,6 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('user1')
         ]);
 
-        $contributor = User::Create([
-            'name' => 'User2',
-            'email' => 'user2@contributor.com',
-            'password' => bcrypt('user2')
-        ]);
-
         $admin->roles()->attach($adminRole);
-        $contributor->roles()->attach($contributorRole);
     }
 }
